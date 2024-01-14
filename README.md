e8213755f67991f5e84464fdf992a7a3e7b6bf3c
Deathlesz <deathless.mcd@gmail.com>
2024-01-14 15:20:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## ResponseError
+Simple utility macros used by [Bromide](https://github.com/Deathlesz/Bromide) for better error handling.
+
+> [!CAUTION]
+> This piece of software is meant to be my personal project.
+> You REALLY don't want to use it in production environment for several reasons (non-exhaustive):
+> * It is made solely for educational purposes
+> * I really suck at Rust
+> * Just look at the code, it's probably awful...
+
+### License
+
+Bromide is distributed under the terms of both the MIT license and the Apache License (Version 2.0).
+
+See [LICENSE-APACHE](LICENSE-APACHE) and [LICENSE-MIT](LICENSE-MIT) for details.
+
+### Contribution
+I _**will not**_ accept any pull requests to this repository. Don't waste your time.
---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+extern crate proc_macro;
+
+use darling::FromVariant;
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::{quote, quote_spanned};
+use syn::{parse_macro_input, spanned::Spanned as _, Data, DataEnum, DeriveInput, Fields};
+
+#[derive(Default, FromVariant)]
+#[darling(default, attributes(response))]
+struct Attributes {
+    status_code: Option<u16>,
+    error_code: Option<String>,
+}
+
+#[proc_macro_derive(ResponseError, attributes(response))]
+pub fn derive(input: TokenStream) -> TokenStream {
+    let DeriveInput {
+        ident: name,
+        data,
+        generics,
+        ..
+    } = parse_macro_input!(input);
+
+    let mut impl_expr = TokenStream2::new();
+
+    match data {
+        Data::Enum(data) => derive_enum(data, &mut impl_expr),
+        _ => {
+            return syn::Error::new(
+                proc_macro2::Span::call_site(),
+                "ResponseError should be derived only on enums",
+            )
+            .to_compile_error()
+            .into()
+        }
+    }
+
+    let (impl_generics, type_generics, where_clause) = generics.split_for_impl();
+
+    let expanded = quote! {
+        impl IntoResponse for #impl_generics #name #type_generics #where_clause {
+            #impl_expr
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+fn derive_enum(data: DataEnum, impl_expr: &mut TokenStream2) {
+    let mut match_expr = TokenStream2::new();
+
+    for variant in data.variants {
+        let variant_name = &variant.ident;
+
+        let fields = match variant.fields {
+            Fields::Unit => quote_spanned!( variant.span()=> ),
+            Fields::Unnamed(_) => quote_spanned!( variant.span()=> (..)),
+            Fields::Named(_) => quote_spanned!( variant.span()=> {..}),
+        };
+
+        let attributes = Attributes::from_variant(&variant).unwrap_or_default();
+
+        let response = match (attributes.status_code, attributes.error_code) {
+            (None, None) => quote_spanned!( variant.span()=> "-1".into_response()),
+            (None, Some(error_code)) => {
+                quote_spanned!( variant.span()=> #error_code.into_response())
+            }
+            (Some(status_code), None) => {
+                quote_spanned!( variant.span()=> axum::http::StatusCode::from_u16(#status_code).expect("invalid status_code").into_response())
+            }
+            (Some(status_code), Some(error_code)) => {
+                quote_spanned!( variant.span()=> (axum::http::StatusCode::from_u16(#status_code).expect("invalid status_code"), #error_code).into_response())
+            }
+        };
+
+        match_expr.extend(quote_spanned! { variant.span()=>
+            Self::#variant_name #fields => #response,
+        });
+    }
+
+    *impl_expr = quote! {
+        fn into_response(self) -> axum::response::Response {
+            match self {
+                #match_expr
+            }
+        }
+    }
+}
